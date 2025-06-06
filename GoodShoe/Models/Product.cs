64a3fa28ef401ac91f95e171fe42e345bbea1ca1
Reena <reena.yong.zn@gmail.com>
2025-06-06 15:51:57 +0800

    Create product model for storing shoe data

---
+++ GoodShoe/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace GoodShoe.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Brand { get; set; }
+        public decimal Price { get; set; }
+        public int Size {  get; set; }
+        public string? Description { get; set; }
+        public int StockCount { get; set; }
+
+        public string? Color { get; set; }
+        public string? Gender { get; set; }
+    }
+}
