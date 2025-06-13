da333eddbfbd95a9f685edf904ee449a1cbc3dac
Reena <reena.yong.zn@gmail.com>
2025-06-13 16:27:10 +0800

    Test change to data model

---
+++ GoodShoe/Models/ShoeItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GoodShoe.Models
+{
+    public class ShoeItem
+    {
+        public int ShoeItemId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a model.")]
+        public string ShoeModelId { get; set; }
+        public Product Product { get; set; }
+
+        public int size { get; set; }
+
+        public int stock_count { get; set; }
+    }
+}
