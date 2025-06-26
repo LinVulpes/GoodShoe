bd2a658e343508e64776be084ef930c18dc43682
Reena <reena.yong.zn@gmail.com>
2025-06-26 18:30:47 +0800

    Updates to admin controller and views for landing page, product list, and CRUD for products

--- GoodShoe/Controllers/AdminController.cs
+++ GoodShoe/Controllers/AdminController.cs
@@ -1,22 +1,94 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GoodShoe.Models;
+using GoodShoe.Data;
 
 namespace GoodShoe.Controllers
 {
     public class AdminController : Controller
     {
+
+        // Establish the context
+        private GoodShoeContext context {  get; set; }
+        public AdminController(GoodShoeContext ctx)
+        {
+            context = ctx;
+        }
+
+        // Landing page for admin view
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Create()
+        // Product List
+        public IActionResult ProdList()
+        {
+            var prod = context.Product
+                .OrderBy(p => p.Name)
+                .ToList();
+            return View(prod);
+        }
+
+        // Order List
+        public IActionResult OrderList()
         {
             return View();
         }
 
-        public IActionResult Delete()
+
+        // Add new product. Opens the edit view but with a new product
+
+        [HttpGet]
+        public IActionResult Create()
         {
-            return View();
+            ViewBag.Action = "Create";
+            return View("Edit", new Product());
+        }
+
+        // Edit product details
+
+        [HttpGet]
+        public IActionResult Edit(int Id)
+        {
+            ViewBag.Action = "Edit";
+            var prod = context.Product.Find(Id);
+            return View(prod);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product prod)
+        {
+            if(ModelState.IsValid)
+            {
+                if (prod.Id == 0)
+                    context.Product.Add(prod);
+                else
+                    context.Product.Update(prod);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                ViewBag.Action = (prod.Id == 0) ? "Add" : "Edit";
+                return View(prod);
+            }
+        }
+
+        // Delete product
+
+        [HttpGet]
+        public IActionResult Delete(int Id)
+        {
+            var prod = context.Product.Find(Id);
+            return View(prod);
+        }
+        [HttpPost]
+        public IActionResult Delete(Product prod)
+        {
+            context.Product.Remove(prod);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Admin");
         }
 
         public IActionResult Details()
@@ -24,9 +96,5 @@ namespace GoodShoe.Controllers
             return View();
         }
 
-        public IActionResult Edit()
-        {
-            return View();
-        }
     }
 }
