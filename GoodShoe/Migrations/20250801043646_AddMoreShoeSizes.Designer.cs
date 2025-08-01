effd35aed1b9787caf28d6c11de35e840437f80a
linvulpes <thetpainghmu112@gmail.com>
2025-08-01 13:14:03 +0800

    Add database update scripts for ProductId restructuring

--- GoodShoe/Migrations/20250801043646_AddMoreShoeSizes.Designer.cs
+++ GoodShoe/Migrations/20250801043646_AddMoreShoeSizes.Designer.cs
@@ -4,6 +4,7 @@ using GoodShoe.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GoodShoe.Migrations
 {
     [DbContext(typeof(GoodShoeDbContext))]
-    partial class GoodShoeContextModelSnapshot : ModelSnapshot
+    [Migration("20250801043646_AddMoreShoeSizes")]
+    partial class AddMoreShoeSizes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
